8f2cdd0cdde138e60eef1352d383b3676db713f1
Emily Li <emily-li@users.noreply.github.com>
2020-05-10 20:59:07 -0700

    Adding animation

--- EnemyController.cs
+++ EnemyController.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
     
 	private Rigidbody2D rigidbody2d;
+    private Animator animator;
 
 	private float speed = 7.5f;
-	private float changeDirectionTimeout = 3f;
+	private float changeDirectionTimeout = 1f;
 	private float directionTimer;
 	private int direction = 1;
 	private bool vertical;
 
 	void Start() {
 		rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
 		directionTimer = changeDirectionTimeout;
 		vertical = true;
 	}
@@ -46,8 +49,14 @@ public class EnemyController : MonoBehaviour {
     private void changePosition() {
     	Vector2 position = rigidbody2d.position;
         if (vertical) {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+
             position.y = position.y + Time.deltaTime * speed * direction;
         } else {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+
             position.x = position.x + Time.deltaTime * speed * direction;
         }
         rigidbody2d.MovePosition(position);
